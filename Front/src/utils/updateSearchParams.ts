38bc295dc0ffdb02888e245f4c93a3358d87495e
Mikita <ghjhjr11@gmail.com>
2025-08-19 18:07:59 +0300

    blanc history bug fix

--- Front/src/utils/updateSearchParams.ts
+++ Front/src/utils/updateSearchParams.ts
@@ -2,7 +2,15 @@ const updateSearchParams = (key: string, value: string | null) => {
   const params = new URLSearchParams(window.location.search);
   if (value) params.set(key, value);
   else {
-    params.set(key, "");
+    if (!params.get(key)) {
+      params.set(key, "");
+      window.history.replaceState(
+        {},
+        "",
+        `${window.location.pathname}?${params.toString()}`
+      );
+      return;
+    } else params.set(key, "");
   }
 
   const newUrl = `${window.location.pathname}?${params.toString()}`;
